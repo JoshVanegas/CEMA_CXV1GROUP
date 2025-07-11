1c52f47f64d47666a260090e90701d9a4ae5c623
Josh Vanegas <vanegasmejiajosepharmando3@gmail.com>
2025-07-11 18:32:48 -0500

    11/07/2025

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,25 +1,7 @@
-import { useState } from 'react'
 import './Principal.css'
 
 function Principal() {
 
-  const conteo =  localStorage.getItem('contador')
-
-  const [contador, setCount] = useState(conteo ? parseInt(conteo) : 0)
-
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => {setCount((contador) => {localStorage.setItem('contador', contador + 1); return contador + 1})}} >
-          contador es {contador}
-        </button>
-        <button onClick={() => {setCount(0); localStorage.removeItem('contador')}}>
-          Reiniciar
-        </button>
-      </div>
-      <p>Soy muy pro en react</p>
-    </>
-  )
 }
 
 export default Principal
